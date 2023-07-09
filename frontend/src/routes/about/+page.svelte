b136452230c02b217c7a87b5b63cba1d8323810b
chrasucar342 <christian.asuerocarrelln6@gmail.com>
2023-07-09 22:51:42 +0200

    Video

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -16,8 +16,20 @@
         <center>
         <h3 class="my-3"> &nbsp; &nbsp; Vídeo de Christian Asuero Carrellán</h3>
         </center>
+        <div class="m-2">
+            <iframe
+                title="SOS2223-JUL-CAC Christian Asuero Carrellán"
+                width="1080"
+                height="720"
+                src="https://youtu.be/gQxPC4ELfHw"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+                style="display: block; margin: 0 auto;"
+            ></iframe>
+        </div>
         <center>
-        <Button style = "margin-top: 10px" color = "primary" href="https://drive.google.com/file/d/14ocLB89IGhqF4W-UNDTQPMhLQB1Mm5zg/view?usp=share_link">Vídeo en Google Drive</Button>
+        <Button style = "margin-top: 10px" color = "primary" href="https://drive.google.com/file/d/1EGF3-HkQ-IOEH2glonZYCu67q6l_bMgc/view?usp=sharing">Vídeo en Google Drive</Button>
         </center>
     </div>
 </Container>
